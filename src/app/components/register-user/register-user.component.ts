8cd4a0f5d890719025d169daeb2cbe81108f9e37
chanakya <chanakya.net@gmail.com>
2025-03-13 19:55:31 +0530

    updated user form

--- src/app/components/register-user/register-user.component.ts
+++ src/app/components/register-user/register-user.component.ts
@@ -1,10 +1,69 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormArray, FormControl } from '@angular/forms';
+import { CreateUser } from '../../models/create-user';
+import { FloatLabelType } from '@angular/material/form-field';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatError } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register-user',
   templateUrl: './register-user.component.html',
   styleUrls: ['./register-user.component.css'],
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatError
+  ]
 })
 export class RegisterUserComponent {
-  // Registration logic will be added here
+  registerForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      mobile: ['', [Validators.required, Validators.pattern('[0-9]{10}')]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
+      roles: this.fb.array([], Validators.required)
+    }, { validator: this.passwordMatchValidator });
+  }
+  readonly hideRequiredControl = new FormControl(false);
+  readonly floatLabelControl = new FormControl('auto' as FloatLabelType);
+  readonly options: any;
+  protected readonly hideRequired = toSignal(this.hideRequiredControl.valueChanges);
+  protected readonly floatLabel = toSignal(
+    this.floatLabelControl.valueChanges.pipe(map(v => v || 'auto')),
+    { initialValue: 'auto' },
+  );
+  passwordMatchValidator(form: FormGroup) {
+    return form.get('password')?.value === form.get('confirmPassword')?.value
+      ? null : { mismatch: true };
+  }
+
+  onRoleChange(event: any, role: string) {
+    const roles = this.registerForm.get('roles') as FormArray;
+    if (event.checked) {
+      roles.push(this.fb.control(role));
+    } else {
+      const index = roles.controls.findIndex(x => x.value === role);
+      roles.removeAt(index);
+    }
+  }
+
+  register() {
+    if (this.registerForm.valid) {
+      const userData: CreateUser = this.registerForm.value;
+      // Implement registration logic
+    }
+  }
 }
--- src/app/models/create-user.ts
+++ src/app/models/create-user.ts
@@ -1,4 +1,4 @@
-export interface CreateUse {
+export interface CreateUser {
   firstName: string;
   lastName: string;
   email: string;
