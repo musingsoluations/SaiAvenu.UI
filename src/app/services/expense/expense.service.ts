02bb668f6ef2c1fb1cb5a5ece0a37784647cb136
chanakya <chanakya.net@gmail.com>
2025-03-26 16:19:27 +0530

    refactor(expense-service): replace mock API calls with real HTTP requests
    Update the ExpenseService to use real HTTP requests instead of mock data. This change integrates the service with the actual backend API, enabling live data interaction for expense creation and retrieval. The environment configuration is injected to dynamically set the API URL.

--- src/app/services/expense/expense.service.ts
+++ src/app/services/expense/expense.service.ts
@@ -1,20 +1,25 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { CreateExpenseDto, ExpenseDto } from '../../models/expense';
 import { MockExpenseService } from './mock-expense.service';
+import { Environment } from '../../../environments/environment.interface';
+import { API_ENV } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExpenseService {
   constructor(private http: HttpClient) { }
+  private readonly environment: Environment = inject(API_ENV);
 
   createExpense(expense: CreateExpenseDto): Observable<string> {
-    return of('mock-expense-id');
+    const url = `${this.environment.apiUrl}/api/Expense/create`;
+    return this.http.post<string>(`${url}`, expense);
   }
 
   getExpenses(month: number, year: number): Observable<ExpenseDto[]> {
-    return of(MockExpenseService.getExpenses(month, year));
+    const url = `${this.environment.apiUrl}/api/Expense/monthly`;
+    return this.http.get<ExpenseDto[]>(`${url}?month=${month}&year=${year}`);
   }
 }
